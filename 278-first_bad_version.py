9c52e5c259ea4e4bcad5e9e1a8107401b7f77751
WweiL <z920631580@gmail.com>
2018-08-16 17:17:29 +0800

    boring binary search

---
+++ 278-first_bad_version.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def binSearch(low, hi):
+            middle = int((low+hi)/2)
+            if middle == low:
+                return low if isBadVersion(low) else hi
+            
+            if isBadVersion(middle):
+                return binSearch(low, middle)
+            else:
+                return binSearch(middle, hi)
+            
+        return binSearch(1, n)
