8a1bbdf2936e6f58dc0e6766ebd85a72ce6bfb4a
WweiL <z920631580@gmail.com>
2018-08-22 15:28:57 +0800

    linked list, backtracking

--- 19-remove_nth_node_from_end_of_list.py
+++ 19-remove_nth_node_from_end_of_list.py
@@ -14,10 +14,13 @@ class Solution:
         sentinel = ListNode(0)
         sentinel.next = head
         def remove(head, n):
+	       # The node is the last node, so return 0.
             if not head.next:
                 return 0
             else:
+		        # keep recording the position
                 loc = 1 + remove(head.next, n)
+		        # if it is the position we want, do the deletion
                 if loc == n:
                     head.next = head.next.next
                 return loc
