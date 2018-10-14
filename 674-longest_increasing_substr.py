a933c62bfcbf5876a2e0c6aa2901a10275401480
WweiL <z920631580@gmail.com>
2018-10-14 10:17:10 -0500

    two pointers

--- 674-longest_increasing_substr.py
+++ 674-longest_increasing_substr.py
@@ -4,12 +4,13 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) <= 1:
-            return len(nums)
+        n = len(nums)
+        if n <= 1:
+            return n
         left = 0
         right = 1
         res = 2 if nums[right] > nums[left] else 1
-        while right < len(nums):
+        while right < n:
             if nums[right-1] < nums[right]:
                 res = max(res, right - left + 1)
             else:
