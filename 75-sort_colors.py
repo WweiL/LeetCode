4f5bb918ee3017b493e039903a7a99a55241790d
WweiL <z920631580@gmail.com>
2018-10-18 16:07:38 -0500

    two pointers, more ways

--- 75-sort_colors.py
+++ 75-sort_colors.py
@@ -15,4 +15,12 @@ class Solution:
             if n == 0:
                 nums[zero] = 0
                 zero += 1
-        
+# void sortColors(int A[], int n) {
+#     int j = 0, k = n-1;
+#     for (int i=0; i <= k; i++) {
+#         if (A[i] == 0)
+#             swap(A[i], A[j++]);
+#         else if (A[i] == 2)
+#             swap(A[i--], A[k--]);
+#     }
+# }
