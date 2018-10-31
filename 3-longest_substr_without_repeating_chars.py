0b6a812062df8bc771c519b3b653627a658126c1
WweiL <z920631580@gmail.com>
2018-10-31 14:31:12 -0500

    hash table + 2 pointers, minor optimization, different thought process

--- 3-longest_substr_without_repeating_chars.py
+++ 3-longest_substr_without_repeating_chars.py
@@ -4,15 +4,26 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        from collections import defaultdict
-        d = defaultdict(int)
-        start = end = 0
-        ans = 0
+        char_pos = {}
+        start, end = 0, 0
+        s = s + "@"
+        ret = 0
         while end < len(s):
-            d[s[end]] += 1
-            while d[s[end]] == 2:
-                d[s[start]] -= 1
-                start += 1
+            ret = max(ret, end-start)
+            if s[end] in char_pos:
+                start = max(char_pos[s[end]] + 1, start)
+            char_pos[s[end]] = end
             end += 1
-            ans = max(ans, end-start)
-        return ans
+        return ret
+        # from collections import defaultdict
+        # d = defaultdict(int)
+        # start = end = 0
+        # ans = 0
+        # while end < len(s):
+        #     d[s[end]] += 1
+        #     while d[s[end]] == 2:
+        #         d[s[start]] -= 1
+        #         start += 1
+        #     end += 1
+        #     ans = max(ans, end-start)
+        # return ans
