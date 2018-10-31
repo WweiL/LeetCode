111960c8f20349b04501d51aeef06b4e724037f8
WweiL <z920631580@gmail.com>
2018-10-30 21:04:27 -0500

    faster! two pointer

--- 283-move_zero.py
+++ 283-move_zero.py
@@ -5,10 +5,20 @@ class Solution:
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         pos = 0
+        zero_cnt = 0
         n = len(nums)
         for i in range(n):
             if nums[i] != 0:
-                temp = nums[i]
-                nums[i] = 0
-                nums[pos] = temp
+                nums[pos] = nums[i]
                 pos += 1
+            else:
+                zero_cnt += 1
+        for i in range(n-1, n-zero_cnt-1, -1):
+            nums[i] = 0
+        # pos = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         temp = nums[i]
+        #         nums[i] = 0
+        #         nums[pos] = temp
+        #         pos+= 1
