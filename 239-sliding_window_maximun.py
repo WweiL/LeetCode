331e11abef99af6ca6166d5958ef76f52b418073
WweiL <z920631580@gmail.com>
2018-11-07 11:40:48 -0600

    单调栈, clearer code

--- 239-sliding_window_maximun.py
+++ 239-sliding_window_maximun.py
@@ -5,29 +5,25 @@ class Solution:
         :type k: int
         :rtype: List[int]
         """
-        from collections import deque
-        if not nums:
+        q = collections.deque()
+        # [max -> min]
+        if not nums or k == 0:
             return []
-        if k == 0:
-            return nums
+        nums.append(-float('inf'))
+        l, r, n = 0, 0, len(nums)
         ans = []
-        q = deque()
-        
-        for i in range(k):
-            while q and nums[i] >= nums[q[-1]]:
-                q.pop()
-            q.append(i)
-            
-        ans.append(nums[q[0]])
-        start = 0
-        end = k
-        while end < len(nums):
-            if start == q[0]:
-                q.popleft()
-            while q and nums[end] >= nums[q[-1]]:
-                q.pop()
-            q.append(end)
-            ans.append(nums[q[0]])
-            end += 1
-            start += 1
+        while r < n:
+            if r - l < k:
+                while q and nums[q[-1]] <= nums[r]:
+                    q.pop()
+                q.append(r)
+                r += 1
+            else:
+                ans.append(nums[q[0]])
+                while q and nums[q[-1]] <= nums[r]:
+                    q.pop()
+                q.append(r)
+                if q[0] == l:
+                    q.popleft()
+                l, r = l+1, r+1
         return ans
