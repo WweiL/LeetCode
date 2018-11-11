3b5f44eef813c25f2521216c7f1a7c0e6395c3cc
WweiL <z920631580@gmail.com>
2018-11-11 16:34:08 -0600

    next premutation, revisited, bin search in second iteration

--- 31-next_permutation.py
+++ 31-next_permutation.py
@@ -14,13 +14,22 @@ cccclass Solution:
         if p == n:
             nums.reverse()
         else:
-            to_swap = float('inf')
-            to_swap_idx = n-1
-            for i in range(n-1, p, -1):
-                if nums[i] > nums[p] and nums[i] < to_swap:
-                    to_swap = nums[i]
-                    to_swap_idx = i
-            
+            # linear search, not good
+            # to_swap = float('inf')
+            # to_swap_idx = n-1
+            # for i in range(n-1, p, -1):
+            #     if nums[i] > nums[p] and nums[i] < to_swap:
+            #         to_swap = nums[i]
+            #         to_swap_idx = i
+            # binary search
+            l, r = p+1, n-1
+            while l < r:
+                mid = l+r+1 >> 1
+                if nums[mid] <= nums[p]:
+                    r = mid - 1
+                else:
+                    l = mid
+            to_swap_idx = l
             nums[to_swap_idx], nums[p] = nums[p], nums[to_swap_idx]
 
             # for i in range(n-1, (n+p)//2, -1):
