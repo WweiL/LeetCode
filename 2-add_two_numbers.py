18594ad08cbaa29d8ca783611ba00736fd0ebd5e
phoebe <phoebeT@gmail.com>
2019-09-28 10:54:12 +0900

    rewrite

--- 2-add_two_numbers.py
+++ 2-add_two_numbers.py
@@ -5,34 +5,20 @@
 #         self.next = None
 
 class Solution:
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if not(l1 and l2):
-            return None
-        
-        
-        head = ListNode((l1.val + l2.val) % 10)
-        c_out = (l1.val + l2.val) // 10
-        l1 = l1.next
-        l2 = l2.next
-        curr = head
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(0)
+        carryout, l = 0, head
         while l1 or l2:
-            val1 = val2 = 0
+            v1 = v2 = 0
             if l1:
-                val1 = l1.val
+                v1 = l1.val
                 l1 = l1.next
             if l2:
-                val2 = l2.val
+                v2 = l2.val
                 l2 = l2.next
-            val = (val1+val2 + c_out) % 10
-            c_out = (val1+val2 + c_out) // 10
-            curr.next = ListNode(val)
-            curr = curr.next
-        
-        if c_out:
-            curr.next = ListNode(c_out)
-        return head
+            l.next = ListNode((v1 + v2 + carryout) % 10)
+            carryout = (v1 + v2 + carryout) // 10
+            l = l.next
+        if carryout:
+            l.next = ListNode(carryout)
+        return head.next
