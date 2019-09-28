7870c15704f9a980f930525776c06e8bef61fc02
phoebe <phoebeT@gmail.com>
2019-09-28 12:07:23 +0900

    new sol exploits symmetry

--- 688-knight_probability_in_chessboard.py
+++ 688-knight_probability_in_chessboard.py
@@ -27,3 +27,25 @@ class Solution:
                     prob[k][i][j] = calcProb(i, j, k, prob)
  
         return prob[K][r][c]
+
+
+class Solution:
+    def knightProbability(self, N: int, K: int, r: int, c: int) -> float:
+        memo = {}
+        return self.dfs(r, c, memo, K, N) / (8**K)
+            
+    def dfs(self, r: int, c: int, memo: dict, m: int, N: int) -> int:
+        # return the number of valid moves starting in (r, c) with m moves left
+        if not (r >= 0 and c >= 0 and r < N and c < N): return 0
+        if m == 0: return 1
+        if (r, c, m) in memo: return memo[(r, c, m)]
+        if (r, N-1-c, m) in memo: return memo[(r, N-1-c, m)]
+        if (N-1-r, c, m) in memo: return memo[(N-1-r, c, m)]
+        if (N-1-r, N-1-c, m) in memo: return memo[(N-1-r, N-1-c, m)]
+        moves = [(1, 2), (1, -2), (-1, -2), (-1, 2), (2, 1), (2, -1), (-2, -1), (-2, 1)]
+        valid_moves = 0
+        for i, j in moves:
+            valid = self.dfs(r+i, c+j, memo, m-1, N)
+            valid_moves += valid
+        memo[(r, c, m)] = valid_moves
+        return valid_moves
